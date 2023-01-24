9109b639b67d5ffb96103477c998d690834f690a
Sai Prasad Sabeson <saiprasad2k6@gmail.com>
2023-01-24 11:29:59 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (56.14%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+// Morris Traversal SC=O(1)
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> resultList = new ArrayList<Integer>();
@@ -23,11 +24,40 @@ class Solution {
         return resultList;
     }
     
-    private void traverse(TreeNode node, List<Integer> resultList){
-        if(node == null) return;
+    private void traverse(TreeNode curr, List<Integer> resultList){
+        TreeNode pred = null;
         
-        traverse(node.left,resultList);
-        resultList.add(node.val);
-        traverse(node.right, resultList);
+        while(curr != null){
+            //When hitting the left most part of the tree, print the value and move right
+            if(curr.left == null){
+                resultList.add(curr.val);
+                curr = curr.right;
+                continue;
+            } 
+            //If the left is not null, find the inorder predecessor
+            pred = findPredecessor(curr);
+            
+            //Inorder predecessor will be null if it has not been visited
+            // If so then create a back link to the current node and move left
+            if(pred.right == null){
+                pred.right = curr;
+                curr = curr.left;    
+            }else{
+                // if Inorder predecessor has already been visited, then 
+                // remove the backlink and then move right of the current node
+                resultList.add(curr.val);
+                pred.right = null;
+                curr = curr.right;
+            }
+                
+        }
+    }
+    
+    private TreeNode findPredecessor(TreeNode root){
+        TreeNode curr = root.left;
+        while(curr.right !=null && curr.right != root){
+            curr = curr.right;
+        }
+        return curr;
     }
 }
