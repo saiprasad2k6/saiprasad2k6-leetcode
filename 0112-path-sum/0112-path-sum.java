198f050c496555db152c37665f42facbbe132e0c
Sai Prasad Sabeson <saiprasad2k6@gmail.com>
2023-02-14 07:34:04 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (57.55%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -16,20 +16,19 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
 
+        //Edge Case - When the given input is null, then there is no point in finding the path. So false
         if (root == null) {
             return false;
         }
-
+        // Base Case - If Left and Right is null, its a leaf. If the path sum to the leaf is != targetSum, then its false.
         if (root.left == null && root.right == null) {
-            
-            if(root.val == targetSum){
+            if (root.val == targetSum) {
                 return true;
-            }else{
-                return false;
             }
+            return false;
         }
-        
 
+        // We navigate revursively, until the leaf is reach and checked for targetSum
         return (hasPathSum(root.left, targetSum - root.val) ||
                 hasPathSum(root.right, targetSum - root.val));
     }
