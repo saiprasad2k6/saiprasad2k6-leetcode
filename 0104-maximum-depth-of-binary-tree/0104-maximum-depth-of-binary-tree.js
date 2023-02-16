61b4c4805575d504782f129bc93b9c1dc7a30aa6
Sai Prasad Sabeson <saiprasad2k6@gmail.com>
2023-02-16 07:03:17 +0530

    Time: 73 ms (60.87%), Space: 44.8 MB (55.48%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -1,27 +1,22 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
-class Solution {
-    public int maxDepth(TreeNode root) {
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
 
-        return findMaxDepth(root, 0);
+    const findDepth = function (node, depth) {
+        if (!node) return depth;
+
+        return Math.max(findDepth(node.left, depth + 1), findDepth(node.right, depth + 1));
     }
 
-    private int findMaxDepth(TreeNode node, int depth) {
-        if (node == null) return depth;
-        return Math.max(findMaxDepth(node.left, depth + 1), findMaxDepth(node.right, depth + 1));
-
-    }
-}
+    return findDepth(root, 0);
+};
