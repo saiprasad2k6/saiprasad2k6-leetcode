22ca7baeb1b0e1cd2b411d5c9a59884bd26bae8d
Sai Prasad Sabeson <saiprasad2k6@gmail.com>
2023-02-19 21:17:14 +0530

    Time: 5 ms (99.80%), Space: 46 MB (43.41%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int MAX = m + n;
+
+        //Top to Bottom, Left to Right
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != 0) {
+                    int top = (i - 1 >= 0) ? mat[i - 1][j] : MAX;
+                    int left = (j - 1 >= 0) ? mat[i][j - 1] : MAX;
+                    mat[i][j] = Math.min(top, left) + 1;
+                }
+            }
+        }
+
+        //Bottom to Top - Right to Left
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (mat[i][j] != 0) {
+                    int bottom = (i + 1 < m) ? mat[i + 1][j] : MAX;
+                    int right = (j + 1 < n) ? mat[i][j + 1] : MAX;
+                    mat[i][j] = Math.min(mat[i][j], Math.min(bottom, right) + 1);
+                }
+            }
+        }
+        return mat;
+    }
+}
