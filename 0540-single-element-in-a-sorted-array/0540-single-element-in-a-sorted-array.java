8ad7ad6dc0e71a80df4677763528dabb0323c2db
Sai Prasad Sabeson <saiprasad2k6@gmail.com>
2023-02-21 10:11:41 +0530

    Time: 0 ms (100.00%), Space: 47.9 MB (93.20%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,32 +1,28 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        //Edge Cases
         if (nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
+
         int n = nums.length;
         int start = 0, end = n - 1, mid = 0;
 
         while (start <= end) {
             mid = start + (end - start) / 2;
-            //System.out.println("start=" + start + " mid=" + mid + " end=" + end);
-            //When the Mid is same as next element
-            if (start == end) return nums[mid];
-            else if (nums[mid] == nums[mid + 1]) {
-                //if length of right is even, go left
-                if ((end - mid + 1) % 2 == 0) {
+            if (start == end) { // When Start is same as End - there are no more iterations further.
+                return nums[mid];
+            } else if (nums[mid] == nums[mid + 1]) {     //When the Mid is same as next element
+                if ((end - mid + 1) % 2 == 0) { //if length of right is even, go left
                     end = mid - 1;
                 } else {
-                    //if length of right is odd
-                    start = mid + 2;
+                    start = mid + 2; //if length of right is odd
                 }
 
             } else if (nums[mid] == nums[mid - 1]) { //When mid is same as Prev
-
-                //if length of right is even
-                if ((end - mid + 1) % 2 != 0) {
+                if ((end - mid + 1) % 2 != 0) { //if length of right is even
                     end = mid - 2;
                 } else {
-                    //if length of right is odd
-                    start = mid + 1;
+                    start = mid + 1; //if length of right is odd
                 }
             } else {
                 return nums[mid];
